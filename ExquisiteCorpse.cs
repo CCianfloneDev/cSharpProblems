6f60d033dda6a0c3a6fdb51ec5d5c2fd3239e793
Cole Cianflone <colecianflone123@gmail.com>
2021-12-24 11:14:25 -0600

    Add ExquisiteCorpse.cs
    Randomly generates an ASCII figure.

---
+++ ExquisiteCorpse.cs
@@ -0,0 +1,153 @@
+using System;
+/** Codecademy Learn-C# Project: Exquisite Corpse
+ * Purpose: Demonstrate understanding of switch statements, and methods
+ * 
+ * Author: Cole Cianflone
+ * Date: Dec 19, 2021
+ */
+namespace ExquisiteCorpse
+{
+    class BuildACreature
+    {
+        static void Main(string[] args)
+        {
+            RandomMode();
+        }
+
+        static void BuildCreature(string head, string body, string feet)
+        {
+            int headNum = TranslateToNumber(head);
+            int bodyNum = TranslateToNumber(body);
+            int feetNum = TranslateToNumber(feet);
+
+            SwitchCase(headNum, bodyNum, feetNum);
+        }
+
+        static void RandomMode()
+        {
+            Random randomNumber = new Random();
+            int randNum1 = randomNumber.Next(1, 4);
+            int randNum2 = randomNumber.Next(1, 4);
+            int randNum3 = randomNumber.Next(1, 4);
+
+            SwitchCase(randNum1, randNum2, randNum3);
+        }
+
+        static void SwitchCase(int head, int body, int feet)
+        {
+            switch (head)
+            {
+                case 1:
+                    GhostHead();
+                    break;
+                case 2:
+                    BugHead();
+                    break;
+                case 3:
+                    MonsterHead();
+                    break;
+                default: break;
+            }
+            switch (body)
+            {
+                case 1:
+                    GhostBody();
+                    break;
+                case 2:
+                    BugBody();
+                    break;
+                case 3:
+                    MonsterBody();
+                    break;
+                default: break;
+            }
+            switch (feet)
+            {
+                case 1:
+                    GhostFeet();
+                    break;
+                case 2:
+                    BugFeet();
+                    break;
+                case 3:
+                    MonsterFeet();
+                    break;
+                default: break;
+            }
+        }
+
+        static int TranslateToNumber(string creaturePart)
+        {
+            switch (creaturePart)
+            {
+                case "ghost": return 1;
+                case "bug": return 2;
+                case "monster": return 3;
+                default: return 1;
+            }
+        }
+
+        static void GhostHead()
+        {
+            Console.WriteLine("     ..-..");
+            Console.WriteLine("    ( o o )");
+            Console.WriteLine("    |  O  |");
+        }
+
+        static void GhostBody()
+        {
+            Console.WriteLine("    |     |");
+            Console.WriteLine("    |     |");
+            Console.WriteLine("    |     |");
+        }
+
+        static void GhostFeet()
+        {
+            Console.WriteLine("    |     |");
+            Console.WriteLine("    |     |");
+            Console.WriteLine("    '~~~~~'");
+        }
+
+        static void BugHead()
+        {
+            Console.WriteLine("     /   \\");
+            Console.WriteLine("     \\. ./");
+            Console.WriteLine("    (o + o)");
+        }
+
+        static void BugBody()
+        {
+            Console.WriteLine("  --|  |  |--");
+            Console.WriteLine("  --|  |  |--");
+            Console.WriteLine("  --|  |  |--");
+        }
+
+        static void BugFeet()
+        {
+            Console.WriteLine("     v   v");
+            Console.WriteLine("     *****");
+        }
+
+        static void MonsterHead()
+        {
+            Console.WriteLine("     _____");
+            Console.WriteLine(" .-,;='';_),-.");
+            Console.WriteLine("  \\_\\(),()/_/");
+            Console.WriteLine("　  (,___,)");
+        }
+
+        static void MonsterBody()
+        {
+            Console.WriteLine("   ,-/`~`\\-,___");
+            Console.WriteLine("  / /).:.('--._)");
+            Console.WriteLine(" {_[ (_,_)");
+        }
+
+        static void MonsterFeet()
+        {
+            Console.WriteLine("    |  Y  |");
+            Console.WriteLine("   /   |   \\");
+            Console.WriteLine("   \"\"\"\" \"\"\"\"");
+        }
+    }
+}
