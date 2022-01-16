2c24471d04ab0b9047e4a50879a5d2f5b91b811a
Cole Cianflone <colecianflone123@gmail.com>
2022-01-16 12:30:40 -0600

    Updated Program.cs

--- ZooKeepingSystem/Program.cs
+++ ZooKeepingSystem/Program.cs
@@ -12,25 +12,55 @@ namespace ZooKeepingSystem
         static void Main(string[] args)
         {
             // declaring new list to pass to animals
-            List<Species> listOfAnimals = new List<Species> {Species.Monkey, Species.Penguin, Species.Giraffe, Species.Tiger, Species.Gorilla};
-            Animals animals = new Animals(listOfAnimals);
+            List<Species> listOfAnimals = new List<Species>();
+
+            foreach (Species s in Enum.GetValues(typeof(Species)))
+            {
+                listOfAnimals.Add(s);
+            }
             
+            Animals animals = new Animals(listOfAnimals);
             // initializing Zoo with given number of cages, zoo keepers, and list of animals.
-            Zoo AssinaboineZoo = new Zoo(5, 6, animals);
+            Zoo AssinaboineZoo = new Zoo(4, 5, animals);
 
-            Console.WriteLine(AssinaboineZoo.ToString());
-            Console.WriteLine($"Animals at zoo: ");
-            animals.PrintSpecies();
+            // initializing menus
+            EmployeeManagement employeeSystem = new EmployeeManagement(AssinaboineZoo);
+            EquipmentManagement equipmentSystem = new EquipmentManagement(AssinaboineZoo);
+            AnimalManagement animalSystem = new AnimalManagement(AssinaboineZoo, animals, listOfAnimals);
 
-            Console.WriteLine("\n\n");
+            Console.WriteLine("ZooKeepingSystem");
+            Console.WriteLine("Designed by: FakeCompany\nPlease choose an option 1 - 3.");
+            Console.WriteLine("Press 9 to exit.");
+            Console.WriteLine(" Option (1): Employee management\n Option (2): Equipment management\n Option (3): Animal management");
+            
+            do
+            {
+                optionChosen = Convert.ToInt32(Console.ReadLine());
 
-            animals.RemoveSpecies(listOfAnimals[0]);
-            animals.PrintSpecies();
+                switch (optionChosen)
+                {
+                    case 9:
+                        sentinelValueEntered = true;
+                        break;
+                    case 1:
+                        Console.WriteLine("Employee management entered.");
+                        employeeSystem.DisplayMenu();
+                        Console.WriteLine(AssinaboineZoo.GetNumberOfZooKeepers());
+                        break;
+                    case 2:
+                        Console.WriteLine("Equipment management entered.");
+                        equipmentSystem.DisplayMenu();
+                        break;
+                    case 3:
+                        Console.WriteLine("Animal management entered.");
+                        /*WIP - animalSystem.DisplayMenu();*/
+                        break;
+                    default:
+                        Console.WriteLine("Main menu entered.");
+                        break;
+                }
 
-            Console.WriteLine("\n\n");
-
-            animals.AddSpecies(Species.Giraffe);
-            animals.PrintSpecies();
+            } while (!sentinelValueEntered);
 
             Console.WriteLine("Press any key to exit...");
             Console.ReadKey();
