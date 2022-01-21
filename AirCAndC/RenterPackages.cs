57550015b57a5b446a2f34d1a70d00c27bd82e8d
Cole Cianflone <colecianflone123@gmail.com>
2022-01-21 12:00:23 -0600

    Added RenterPackages.cs

---
+++ AirCAndC/RenterPackages.cs
@@ -0,0 +1,23 @@
+namespace AirCandC_api
+{
+    /// <summary>
+    /// Represents different renting packages associated with rental property listing on Air c&c.
+    /// </summary>
+    public enum RenterPackages
+    { 
+        /// <summary>
+        /// Daily package (charged daily).
+        /// </summary>
+        Daily,
+
+        /// <summary>
+        /// Weekly package (charged weekly)
+        /// </summary>
+        Weekly,
+
+        /// <summary>
+        /// Monthly package (charged monthly)
+        /// </summary>
+        Monthly
+    }
+}
