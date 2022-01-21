a30bfcdcf9c3cfa33854f859f927c25a90963688
Cole Cianflone <colecianflone123@gmail.com>
2022-01-21 12:02:54 -0600

    Added Program.cs

---
+++ AirCAndC/Program.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AirCandC_api;
+
+namespace AirCandC
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Amenities> amenities = new List<Amenities>();
+            amenities.Add(Amenities.Pool);
+            amenities.Add(Amenities.MovieRoom);
+            AirCCPropertyListing listing01 = new AirCCPropertyListing(2, 2, 1500, "1080 Fleming avenue", RenterPackages.Weekly, 500, amenities);
+
+            Console.WriteLine(listing01.ToString());
+            Console.WriteLine("Amenities: ");
+            listing01.PrintAmenities();
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+
+        }
+    }
+}
