9da6fe38dc922f0040375ecdb053d26a909253e2
Cole Cianflone <colecianflone123@gmail.com>
2022-01-21 12:00:49 -0600

    Added Amenities.cs

---
+++ AirCAndC/Amenities.cs
@@ -0,0 +1,38 @@
+namespace AirCandC_api
+{
+    /// <summary>
+    /// Represents amenities associated with rental property listing on Air c&c.
+    /// </summary>
+    public enum Amenities
+    {
+        /// <summary>
+        /// Listing has a pool table.
+        /// </summary>
+        PoolTable,
+
+        /// <summary>
+        /// Listing has a ping pong table.
+        /// </summary>
+        PingPongTable,
+        
+        /// <summary>
+        /// Listing has a pool.
+        /// </summary>
+        Pool,
+
+        /// <summary>
+        /// Listing has a hot tub.
+        /// </summary>
+        HotTub,
+
+        /// <summary>
+        /// Listing has a movie room.
+        /// </summary>
+        MovieRoom,
+
+        /// <summary>
+        /// Listing has no amenities.
+        /// </summary>
+        None
+    }
+}
