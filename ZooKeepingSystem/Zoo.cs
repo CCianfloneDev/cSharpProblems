c97f8b035e4cf11f7b88fc1dd0a163f1b2cd8a2f
Cole Cianflone <colecianflone123@gmail.com>
2022-01-28 16:41:58 -0600

    Update Zoo.cs
    Impl Props.

--- ZooKeepingSystem/Zoo.cs
+++ ZooKeepingSystem/Zoo.cs
@@ -7,68 +7,35 @@ namespace ZooKeepingSystem
     /// </summary>
     public class Zoo
     {
-        private Animals animals;
-        private string name;
-        private int numberOfCages;
-        private int numberOfZooKeepers;
+        public int NumberOfCages
+        {
+            get;
+            set;
+        }
+
+        public int NumberOfZooKeepers
+        {
+            get;
+            set;
+        }
+
+        public Animals AnimalsInZoo
+        {
+            get;
+            set;
+        }
 
         /// <summary>
-        /// Initializes a zoo with a name, set number of cages, number of zoo keepers, and list of animals.
+        /// Initializes a zoo with a set number of cages, number of zoo keepers, and list of animals.
         /// </summary>
-        /// <param name="name">The name of the zoo.</param>
         /// <param name="numberOfCages">Number of cages.</param>
         /// <param name="numberOfZooKeepers">Total number of zoo keepers employed.</param>
         /// <param name="animals">Animals.</param>
-        public Zoo(string name, int numberOfCages, int numberOfZooKeepers, Animals animals)
+        public Zoo(int numberOfCages, int numberOfZooKeepers, Animals animals)
         {
-            this.name = name;
-            this.animals = animals;
-            this.numberOfCages = numberOfCages;
-            this.numberOfZooKeepers = numberOfZooKeepers;
-        }
-        
-        /// <summary>
-        /// Initializes a zoo with a name, 0 cages, 1 zoo keeper, and no animals in the care.
-        /// </summary>
-        /// <param name="name">The name of the zoo.</param>
-        public Zoo(string name) : this(name, 0, 1, null)
-        {
-        }
-
-        /// <summary>
-        /// Gets number of cages in zoo.
-        /// </summary>
-        /// <returns>Number of cages.</returns>
-        public int GetNumberOfCages()
-        {
-            return this.numberOfCages;
-        }
-
-        /// <summary>
-        /// Sets number of cages in zoo.
-        /// </summary>
-        /// <param name="numberOfCages">Number of cages.</param>
-        public void SetNumberOfCages(int numberOfCages)
-        {
-            this.numberOfCages = numberOfCages;
-        }
-
-        /// <summary>
-        /// Gets number of zoo keepers employees.
-        /// </summary>
-        /// <returns></returns>
-        public int GetNumberOfZooKeepers()
-        {
-            return this.numberOfZooKeepers;
-        }
-
-        /// <summary>
-        /// Sets number of zoo keepers currently employed.
-        /// </summary>
-        /// <param name="numberOfZooKeepers">Number of zoo keepers.</param>
-        public void SetNumberOfZooKeepers(int numberOfZooKeepers)
-        {
-            this.numberOfZooKeepers = numberOfZooKeepers;
+            NumberOfCages = numberOfCages;
+            NumberOfZooKeepers = numberOfZooKeepers;
+            AnimalsInZoo = animals;
         }
         
         /// <summary>
@@ -77,7 +44,7 @@ namespace ZooKeepingSystem
         /// <returns>String representation.</returns>
         public override string ToString()
         {
-            return $"Number of cages at zoo: {numberOfCages}\nNumber of animals: {animals.GetNumberOfAnimals()}\nTotal zoo keepers employed: {GetNumberOfZooKeepers()}\n";
+            return $"Number of cages at zoo: {NumberOfCages}\nNumber of animals: {AnimalsInZoo}\nTotal zoo keepers employed: {NumberOfZooKeepers}\n";
         }
     }
 }
