d030d923fcf022caaa8e0b776bcf58d8a8c6cd86
Cole Cianflone <colecianflone123@gmail.com>
2022-01-31 09:43:33 -0600

    Update Zoo.cs
    Added documentation and Impl. exception handling.

--- ZooKeepingSystem/Zoo.cs
+++ ZooKeepingSystem/Zoo.cs
@@ -7,28 +7,40 @@ namespace ZooKeepingSystem
     /// </summary>
     public class Zoo
     {
+        /// <summary>
+        /// Gets and sets number of cages.
+        /// </summary>
         public int NumberOfCages
         {
             get;
             set;
         }
 
+        /// <summary>
+        /// Gets and sets number of zoo keepers.
+        /// </summary>
         public int NumberOfZooKeepers
         {
             get;
             set;
         }
 
+        /// <summary>
+        /// Gets and set animals in the zoo.
+        /// </summary>
         public Animals AnimalsInZoo
         {
             get;
             set;
         }
 
+        /// <summary>
+        /// Gets and sets the name of the zoo.
+        /// </summary>
         public string ZooName
         {
             get;
-            set;
+            private set;
         }
 
         /// <summary>
@@ -38,8 +50,27 @@ namespace ZooKeepingSystem
         /// <param name="numberOfZooKeepers">Total number of zoo keepers employed.</param>
         /// <param name="animals">Animals.</param>
         /// <param name="zooName">Name of the zoo.</param>
+        /// <exception cref="ArgumentNullException">
+        /// Thrown when initialized with no name.
+        /// </exception>
+        /// <exception cref="ArgumentOutOfRangeException">
+        /// Thrown when number of cages or number of zoo keepers is less than zero.
+        /// </exception>
         public Zoo(string zooName, int numberOfCages, int numberOfZooKeepers, Animals animals)
         {
+            if (zooName == null)
+            {
+                throw new ArgumentNullException("Name", "Name cannot be null.");
+            }
+            if (numberOfCages < 0)
+            {
+                throw new ArgumentOutOfRangeException("NumberOfCages", "Argument cannot be less than zero.");
+            }
+            if (numberOfZooKeepers < 0)
+            {
+                throw new ArgumentOutOfRangeException("NumberOfZooKeepers", "Argument cannot be less than zero.");
+            }
+
             ZooName = zooName;
             NumberOfCages = numberOfCages;
             NumberOfZooKeepers = numberOfZooKeepers;
@@ -50,7 +81,7 @@ namespace ZooKeepingSystem
         /// Initializes a zoo with a name, 0 cages, 1 zoo keeper, and no animals in the care.
         /// </summary>
         /// <param name="zooName">The name of the zoo.</param>
-        public Zoo(string zooName) : this(zooName, 0, 1, null)
+        public Zoo(string zooName) : this(zooName, 1, 1, null)
         {
         }
 
