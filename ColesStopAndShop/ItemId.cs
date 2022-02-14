990265589a194305a4d093004ba26cc009ba6c05
Cole Cianflone <colecianflone123@gmail.com>
2022-02-14 13:12:44 -0600

    Add files via upload

---
+++ ColesStopAndShop/ItemId.cs
@@ -0,0 +1,27 @@
+﻿namespace ColesStopAndShop
+{
+    /// <summary>
+    /// Represents all items that locations of Cole's-Stop-And-Shop franchise can sell. 
+    /// </summary>
+    public enum ItemId
+    {
+        HoneyBuns = 1000,
+
+        Jerky = 1001,
+
+        JerkyStick = 1002,
+
+        Apple = 1003,
+
+        Banana = 1004,
+
+        Gatorade = 1005,
+
+        Crackers = 1006,
+
+        Slurpee = 1007,
+
+        Chips = 1008
+
+    }
+}
