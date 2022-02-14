85bfbc385daee035e6a9bbd29f91c81628bc9429
Cole Cianflone <colecianflone123@gmail.com>
2022-02-14 15:35:24 -0600

    Add files via upload

---
+++ ColesStopAndShop/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ColesStopAndShop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<ItemId> items = new List<ItemId>();
+            items.Add((ItemId)1000);
+
+            CashRegister cashRegister = new CashRegister(0, 0, 42069);
+
+            cashRegister.ProcessPayment(true);
+
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
