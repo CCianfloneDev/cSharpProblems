a79963c05e263fa9559556128f0b671d54b8f499
Cole Cianflone <colecianflone123@gmail.com>
2022-02-15 13:48:45 -0600

    Update ItemId.cs
    added more items + documentation.

--- ColesStopAndShop/ItemId.cs
+++ ColesStopAndShop/ItemId.cs
@@ -1,27 +1,68 @@
-﻿namespace ColesStopAndShop
+namespace ColesStopAndShop
 {
     /// <summary>
     /// Represents all items that locations of Cole's-Stop-And-Shop franchise can sell. 
     /// </summary>
     public enum ItemId
     {
+        /// <summary>
+        /// Honey buns - item id: 1000.
+        /// </summary>
         HoneyBuns = 1000,
 
+        /// <summary>
+        /// Jerky - item id: 1001.
+        /// </summary>
         Jerky = 1001,
 
+        /// <summary>
+        /// Jerky sticks - item id: 1002.
+        /// </summary>
         JerkyStick = 1002,
 
+        /// <summary>
+        /// Apple - item id: 1003.
+        /// </summary>
         Apple = 1003,
 
+        /// <summary>
+        /// Banana - item id: 1004.
+        /// </summary>
         Banana = 1004,
 
+        /// <summary>
+        /// Gatorade - item id: 1005.
+        /// </summary>
         Gatorade = 1005,
 
+        /// <summary>
+        /// Crackers - item id: 1006.
+        /// </summary>
         Crackers = 1006,
 
+        /// <summary>
+        /// Slurpee - item id: 1007.
+        /// </summary>
         Slurpee = 1007,
 
-        Chips = 1008
+        /// <summary>
+        /// Chips - item id: 1008.
+        /// </summary>
+        Chips = 1008,
 
+        /// <summary>
+        /// Hotdog - HotFood - item id: 1009.
+        /// </summary>
+        Hotdog = 1009,
+
+        /// <summary>
+        /// Taquito - HotFood - item id: 1010.
+        /// </summary>
+        Taquito = 1010,
+
+        /// <summary>
+        /// Chocolate bar - item id: 1011.
+        /// </summary>
+        ChocolateBar = 1011
     }
 }
