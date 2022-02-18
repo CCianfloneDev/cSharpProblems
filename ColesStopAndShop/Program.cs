7783f1235fe8c5c904b649995cfb5a8e4d61e406
Cole Cianflone <colecianflone123@gmail.com>
2022-02-18 15:24:50 -0600

    Update Program.cs

--- ColesStopAndShop/Program.cs
+++ ColesStopAndShop/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,12 +10,26 @@ namespace ColesStopAndShop
     {
         static void Main(string[] args)
         {
-            List<ItemId> items = new List<ItemId>();
-            items.Add((ItemId)1000);
+            // Console.Beep(18000, 1000);
+            // This is all the items that gasStation01 will keep in inventory.
+            Dictionary<ItemId, decimal> allItemsAtStore = new Dictionary<ItemId, decimal>
+            {
+                { ItemId.Slurpee, 2.15m },
+                { ItemId.Apple, 1.15m },
+                { ItemId.Taquito, 2.75m },
+                { ItemId.Gatorade, 3.75m },
+                { ItemId.Banana, 2.25m },
+                { ItemId.HoneyBuns, 2.35m },
+                { ItemId.Crackers, 1.75m }
+            };
 
-            CashRegister cashRegister = new CashRegister(0, 0, 42069);
 
-            cashRegister.ProcessPayment(true);
+            GasStation gasStation01 = new GasStation("109 Asdora", 2, 232, 2, true, false, allItemsAtStore);
+            CashRegister cashRegister = new CashRegister(0, 0, 0, gasStation01, 0.07m, 0.14m);
+
+            cashRegister.ProcessPayment(PaymentType.Debit);
+
+            Console.WriteLine(cashRegister.DebitBalance);
 
             Console.WriteLine("Press any key to exit.");
             Console.ReadKey();
