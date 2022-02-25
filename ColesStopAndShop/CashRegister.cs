d7b6a7ce5d5bdc883eb32f78acb26d575b0cb9d6
Cole Cianflone <colecianflone123@gmail.com>
2022-02-25 12:30:07 -0600

    Update CashRegister.cs
    Added break keyword in item verification step (foreach) to speed up execution time

--- ColesStopAndShop/CashRegister.cs
+++ ColesStopAndShop/CashRegister.cs
@@ -225,6 +225,7 @@ namespace ColesStopAndShop
                         if ((ItemId)itemScanned == itemId)
                         {
                             listOfItems.Add((ItemId)itemScanned);
+                            break;
                         }
                     }
                 }
