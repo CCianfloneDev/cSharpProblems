55eb64450cc67cec5d46f949efb4153a54ba19c5
Cole Cianflone <colecianflone123@gmail.com>
2022-02-28 12:08:46 -0600

    Added FuelType.cs
    Added different options of fuel customers can purchase.

---
+++ ColesStopAndShop/FuelType.cs
@@ -0,0 +1,28 @@
+namespace ColesStopAndShop
+{
+    /// <summary>
+    /// Represents different fuel types available at a gas station.
+    /// </summary>
+    public enum FuelType
+    {
+        /// <summary>
+        /// Regular fuel 87 octane.
+        /// </summary>
+        RegularGasoline = 001,
+
+        /// <summary>
+        /// Mid-grade fuel 90 octane.
+        /// </summary>
+        MidgradeGasoline = 002,
+
+        /// <summary>
+        /// Premium fuel 94 octane.
+        /// </summary>
+        PremiumGasoline = 003,
+
+        /// <summary>
+        /// Diesel fuel.
+        /// </summary>
+        Diesel = 004
+    }
+}
